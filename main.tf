9fe13f6faf6dd4a56607d7eca140e710f181a2d1
Martin Castro <martincastro.cm@gmail.com>
2020-02-12 15:22:53 -0300

    requiered changes added

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ terraform {
 locals {
   headers = "${
     map(
-      "Access-Control-Allow-Headers"    , "'${join(",", var.allow_headers)}'",
-      "Access-Control-Allow-Methods"    , "'${join(",", var.allow_methods)}'",
-      "Access-Control-Allow-Origin"     , "'${var.allow_origin}'",
-      "Access-Control-Max-Age"          , "'${var.allow_max_age}'",
+      "Access-Control-Allow-Headers", "'${join(",", var.allow_headers)}'",
+      "Access-Control-Allow-Methods", "'${join(",", var.allow_methods)}'",
+      "Access-Control-Allow-Origin", "'${var.allow_origin}'",
+      "Access-Control-Max-Age", "'${var.allow_max_age}'",
       "Access-Control-Allow-Credentials", "${var.allow_credentials ? "'true'" : ""}"
     )
   }"
@@ -46,22 +46,37 @@ locals {
   )}"
 }
 
+data "template_file" "template" {
+    count = length(var.lambda_code_file_template) > 0 ? 1 : 0
+    template = file("${var.lambda_code_path}/${var.lambda_code_file_template}")
+
+    vars = var.lambda_code_file_template_vars
+}
+
+resource "local_file" "template" {
+    count = length(var.lambda_code_file_template) > 0 ? 1 : 0
+    content  = data.template_file.template[0].rendered
+    filename = "${var.lambda_code_path}/${replace(var.lambda_code_file_template,".tpl","")}"
+}
+
 data "archive_file" "lambda_zip" {
-    type        = "zip"
-    source_dir  = var.lambda_code_path
-    output_path = "${var.lambda_function_name}.zip"
+  type        = "zip"
+  source_dir  = var.lambda_code_path
+  output_path = "${var.lambda_function_name}.zip"
+
+  depends_on = [local_file.template[0]]
 }
 
 resource "aws_lambda_function" "lambda" {
-  filename          = "${var.lambda_function_name}.zip"
-  source_code_hash  = data.archive_file.lambda_zip.output_base64sha256
-  function_name     = var.lambda_function_name
-  role              = aws_iam_role.lambda_role.arn
-  description       = var.lambda_description
-  handler           = var.lambda_handler
-  runtime           = var.lambda_runtime
-  tags              = var.tags
-  
+  filename         = "${var.lambda_function_name}.zip"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  function_name    = var.lambda_function_name
+  role             = aws_iam_role.lambda_role.arn
+  description      = var.lambda_description
+  handler          = var.lambda_handler
+  runtime          = var.lambda_runtime
+  tags             = var.tags
+
   dynamic "environment" {
     for_each = var.environment == null ? [] : [var.environment]
     content {
@@ -97,14 +112,14 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_iam_role_policy_attachment" {
-    count         =   length(var.lambda_policy_arn)
-    role          =   aws_iam_role.lambda_role.name
-    policy_arn    =   var.lambda_policy_arn[count.index] #element(var.lambda_policy_arn, count.index)
+  count      = length(var.lambda_policy_arn)
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = var.lambda_policy_arn[count.index] #element(var.lambda_policy_arn, count.index)
 }
 
 ## Lambda
 resource "aws_lambda_permission" "lambda_permission" {
-  count = length(var.api_gateway_id) > 0 ? 1 : 0  
+  count = length(var.api_gateway_id) > 0 ? 1 : 0
 
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -116,11 +131,11 @@ resource "aws_lambda_permission" "lambda_permission" {
 }
 
 resource "aws_api_gateway_resource" "api_gateway_resource" {
-  count =  length(var.api_gateway_id) > 0  && var.api_gateway_resource_id == "" ? 1 : 0
-  
-  rest_api_id =  var.api_gateway_id
-  parent_id   =  var.api_gateway_root_resource_id
-  path_part   =  var.resource_path
+  count = length(var.api_gateway_id) > 0 && var.api_gateway_resource_id == "" ? 1 : 0
+
+  rest_api_id = var.api_gateway_id
+  parent_id   = var.api_gateway_root_resource_id
+  path_part   = var.resource_path
 }
 
 resource "aws_api_gateway_method" "api_gateway_method" {
@@ -144,8 +159,18 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "api_gateway_deployment" {
+
+resource "aws_api_gateway_method_response" "response" {
   count = length(var.api_gateway_id) > 0 ? 1 : 0
+
+  rest_api_id = var.api_gateway_id
+  resource_id = length(var.api_gateway_resource_id) == 0 ? aws_api_gateway_resource.api_gateway_resource[0].id : var.api_gateway_resource_id
+  http_method = var.request_method
+  status_code = 200
+}
+
+resource "aws_api_gateway_deployment" "api_gateway_deployment" {
+  count      = length(var.api_gateway_id) > 0 ? 1 : 0
   depends_on = [aws_api_gateway_integration.integration]
 
   rest_api_id = var.api_gateway_id
@@ -156,7 +181,7 @@ resource "aws_api_gateway_deployment" "api_gateway_deployment" {
 ## adding CORs support ##
 
 resource "aws_api_gateway_method" "cors_method" {
-  count =  var.cors_enable && length(var.api_gateway_id) > 0  && length(var.api_gateway_resource_id) == 0 ? 1 : 0
+  count = var.cors_enable && length(var.api_gateway_id) > 0 && length(var.api_gateway_resource_id) == 0 ? 1 : 0
 
   rest_api_id   = var.api_gateway_id
   resource_id   = aws_api_gateway_resource.api_gateway_resource[0].id
@@ -166,7 +191,7 @@ resource "aws_api_gateway_method" "cors_method" {
 
 # aws_api_gateway_integration.
 resource "aws_api_gateway_integration" "cors_integration" {
-  count =  var.cors_enable && length(var.api_gateway_id) > 0  && length(var.api_gateway_resource_id) == 0 ? 1 : 0
+  count = var.cors_enable && length(var.api_gateway_id) > 0 && length(var.api_gateway_resource_id) == 0 ? 1 : 0
 
   rest_api_id = var.api_gateway_id
   resource_id = aws_api_gateway_resource.api_gateway_resource[0].id
@@ -181,7 +206,7 @@ resource "aws_api_gateway_integration" "cors_integration" {
 
 # aws_api_gateway_integration_response._
 resource "aws_api_gateway_integration_response" "cors_response" {
-  count =  var.cors_enable && length(var.api_gateway_id) > 0  && length(var.api_gateway_resource_id) == 0 ? 1 : 0
+  count = var.cors_enable && length(var.api_gateway_id) > 0 && length(var.api_gateway_resource_id) == 0 ? 1 : 0
 
   rest_api_id = var.api_gateway_id
   resource_id = aws_api_gateway_resource.api_gateway_resource[0].id
@@ -195,7 +220,7 @@ resource "aws_api_gateway_integration_response" "cors_response" {
 
 # aws_api_gateway_method_response._
 resource "aws_api_gateway_method_response" "cors_method_response" {
-  count =  var.cors_enable && length(var.api_gateway_id) > 0  && length(var.api_gateway_resource_id) == 0 ? 1 : 0
+  count = var.cors_enable && length(var.api_gateway_id) > 0 && length(var.api_gateway_resource_id) == 0 ? 1 : 0
 
   rest_api_id = var.api_gateway_id
   resource_id = aws_api_gateway_resource.api_gateway_resource[0].id
